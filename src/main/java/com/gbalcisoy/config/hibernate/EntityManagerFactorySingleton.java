c47da55fe3ae8fc2e6141a6a8e96516a01151751
gbalcisoy <gbalcisoy@gmail.com>
2019-10-30 05:40:28 +0200

    settgns

--- src/main/java/com/gbalcisoy/config/hibernate/EntityManagerFactorySingleton.java
+++ src/main/java/com/gbalcisoy/config/hibernate/EntityManagerFactorySingleton.java
@@ -3,9 +3,17 @@ package com.gbalcisoy.config.hibernate;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import lombok.Getter;
+import org.hibernate.c3p0.internal.C3P0ConnectionProvider;
+import org.hibernate.dialect.PostgreSQL95Dialect;
+import org.postgresql.Driver;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hibernate.cfg.AvailableSettings.*;
+import static org.hibernate.tool.schema.Action.VALIDATE;
 
 public class EntityManagerFactorySingleton {
 
@@ -20,7 +28,10 @@ public class EntityManagerFactorySingleton {
         this.vertx = vertx;
         this.vertx.executeBlocking(blockingProcessFuture -> {
             try {
-                this.entityManagerFactory = Persistence.createEntityManagerFactory("vertx-hibernate-int-tests");
+                this.entityManagerFactory =
+                        Persistence
+                                .createEntityManagerFactory("vertx-hibernate-int-tests"
+                                        , createJpaConfigMap());
                 blockingProcessFuture.complete();
             } catch (Exception e) {
                 blockingProcessFuture.fail(e);
@@ -34,6 +45,30 @@ public class EntityManagerFactorySingleton {
                 future.fail(asyncResult.cause());
             }
         });
+    }
+
+    private Map createJpaConfigMap() {
+        HashMap<String, Object> jpaConfigMap = new HashMap<>();
+        jpaConfigMap.put(DIALECT, PostgreSQL95Dialect.class);
+        jpaConfigMap.put(GENERATE_STATISTICS, false);
+        jpaConfigMap.put(HBM2DDL_AUTO, VALIDATE);
+        jpaConfigMap.put(SHOW_SQL, true);
+        jpaConfigMap.put(FORMAT_SQL, false);
+        jpaConfigMap.put(USE_SQL_COMMENTS, false);
+        // jdbc props
+        jpaConfigMap.put(JPA_JDBC_DRIVER, Driver.class);
+        jpaConfigMap.put(JPA_JDBC_URL, "jdbc:postgresql://localhost:5432/postgres");
+        jpaConfigMap.put(JPA_JDBC_USER, "postgres");
+        jpaConfigMap.put(JPA_JDBC_PASSWORD, "postgres321");
+        // c3p0 props
+        jpaConfigMap.put(CONNECTION_PROVIDER, C3P0ConnectionProvider.class);
+        jpaConfigMap.put(C3P0_MAX_SIZE, 20);
+        jpaConfigMap.put(C3P0_MIN_SIZE, 1);
+        jpaConfigMap.put(C3P0_ACQUIRE_INCREMENT, 1);
+        jpaConfigMap.put(C3P0_IDLE_TEST_PERIOD, 300);
+        jpaConfigMap.put(C3P0_MAX_STATEMENTS, 0);
+        jpaConfigMap.put(C3P0_TIMEOUT, 100);
+        return jpaConfigMap;
     }
 
     public static EntityManagerFactorySingleton getInstance() {
